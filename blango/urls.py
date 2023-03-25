288511023b5fa0d85beabc4407e53ca178f44a9e
Viktor Nizhenets <nontxt.steam@gmail.com>
2023-03-25 13:26:19 +0000

    Finish custom template tags

--- blango/urls.py
+++ blango/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-import blog.views
+from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", blog.views.index),
+    path("", views.index),
+    path("post/<slug>", views.post_detail, name="blog-post-detail"),
 ]
