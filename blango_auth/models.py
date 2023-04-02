854028dde91eda76435829054db1f06f0b21a579
Viktor Nizhenets <nontxt.steam@gmail.com>
2023-04-02 09:57:47 +0000

    Finish testing DRF with mocks

--- blango_auth/models.py
+++ blango_auth/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, UserManager
 from django.utils.translation import gettext_lazy as _
-
+from django.contrib.auth.hashers import make_password
 
 class BlangoUserManager(UserManager):
   
