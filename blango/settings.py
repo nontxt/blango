52a4acf4dd5bdc7464a6cff2b88b8d7047621789
Viktor Nizhenets <nontxt.steam@gmail.com>
2023-04-02 16:18:45 +0000

    Finish pagination and django filter

--- blango/settings.py
+++ blango/settings.py
@@ -58,6 +58,7 @@ class Dev(Configuration):
       'django.contrib.staticfiles',
       'rest_framework',
       'rest_framework.authtoken',
+      'django_filters',
       'drf_yasg',
       'crispy_forms',
       'crispy_bootstrap5',
@@ -230,7 +231,14 @@ class Dev(Configuration):
         "anon_burst": "10/minute",
         "user_sustained": "5000/day",
         "user_burst": "100/minute",
-        },    
+        },   
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 100, 
+
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.OrderingFilter"
+        ],
     }
 
   SWAGGER_SETTINGS = {
