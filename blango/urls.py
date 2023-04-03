1e50c79b201c584d359bfb6fa8bbde1e611fc545
Viktor Nizhenets <nontxt.steam@gmail.com>
2023-04-03 11:45:58 +0000

    Finish JavaScript intro and fundamentals

--- blango/urls.py
+++ blango/urls.py
@@ -34,6 +34,7 @@ urlpatterns = [
     path("", blog.views.index),
     path("ip/", blog.views.get_ip),
     path("post/<slug>", blog.views.post_detail, name="blog-post-detail"),
+    path("post-table/", blog.views.post_table, name="blog-post-table"),
     path("api/v1/", include("blog.api.urls"))
 ]
 
